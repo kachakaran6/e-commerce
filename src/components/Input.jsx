2621058fcdc3c6130ec18763db69a4c5457f8927
kachakaran6 <kachakaran06@gmail.com>
2025-03-23 18:31:38 +0530

    data js

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Input = () => {
+const Input = ({ handleChange, value, title, name, color }) => {
   return (
     <div>
       <label htmlFor="" className="sidebar-label-container">
-        <input type="radio" name="test2" />
-        <span className="checkmark"></span>All
+        <input onChange={handleChange} type="radio" value={value} name={name} />
+        <span className="checkmark" style={{ backgroundColor: color }}></span>
+        {title}
       </label>
       ;
     </div>
