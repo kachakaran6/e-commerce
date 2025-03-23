904717374768d2ee972c7b506b9c5b2c669a1543
kachakaran6 <kachakaran06@gmail.com>
2025-03-23 16:02:25 +0530

    Recommended added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Nav from "./Navigation/Nav";
 import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
 
 function App() {
   return (
     <div>
       <Nav />
       <Products />
+      <Recommended />
     </div>
   );
 }
